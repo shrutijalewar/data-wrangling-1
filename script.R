63734f8b73910cc9d4a1b0faeb6d8b70c31b4dad
Shruti Sharma <shruti.sharma@ingramcontent.com>
2018-01-06 11:57:05 -0600

    adding exercise answers

--- script.R
+++ script.R
@@ -1,10 +1,27 @@
+install.packages("tidyverse")
 library(tidyverse)
 
 ach_profile <- read_csv("data/achievement_profile_data_with_CORE.csv")
+View(ach_profile)
+summary(ach_profile)
+glimpse(ach_profile)
+
+# select(df,x,y,z)
+# select(df, x:z)
+# select(df, s,d,everything()) you can order the columns and then call eeverything for rest of the columns
+# EL eng learner; SWD=student with disability ;ED= eco disadva;BHN=black,hispanic,nativeAmerican
 
 ## Exercise 1
 # Use filter() to find the number of districts with a 100% Algebra I proficiency rate.
+#
+filter(ach_profile, AlgI==100)
 
+
+# mutate(ach_profile, if_else(Enrollment>= 1000, "Large","Small"))
+# mutate(ach_profile, district_size = if_else(Enrollment>= 10000, "Large","NA_character_")
+# district_size = if_else(Enrollment>= 5000, "medium",district_size)
+# )
+# the second one overrides 1st one in if_else but case when does not over_ride
 
 
 ## Exercise 2
@@ -12,12 +29,18 @@ ach_profile <- read_csv("data/achievement_profile_data_with_CORE.csv")
 #     * `"High"` if a district's Math proficiency is 75% or higher;
 #     * `"Medium"` if a district's Math proficiency is between 50% and 75%;
 #     * `"Low"` if a district's Math proficiency is below 50%.
+mutate(ach_profile, math_achievement=case_when(Math >75 ~ 'High',
+                                               Math >= 50 ~ 'Medium',
+                                               Math < 50 ~ 'Low',
+                                               TRUE ~ 'No Data')
+                                             ) %>% View()
 
+# View(ach_profile)
 
 
 ## Exercise 3
 # Filter down to district 792 (Shelby County), then pipe the result to `View()`.
-
+filter(ach_profile, system == 792) %>% View()
 
 
 ## Exercise 4
@@ -30,30 +53,49 @@ ach_profile <- read_csv("data/achievement_profile_data_with_CORE.csv")
 #     * `District-Wide: Numeracy` to `TVAAS Numeracy`.
 # 3. Drop the `District Name` variable.
 
+tvaas <- read_csv('data/tvaas.csv') %>%
+    rename(system ='District Number',
+           'TVAAS Composite' = 'District-Wide: Composite',
+           'TVAAS Numeracy' = 'District-Wide: Numeracy') %>%
+    select(-`District Name`)
+
+View(tvaas)
 
 
 ## Exercise 5
 # Sort alphabetically by CORE region, then by Algebra I proficiency in descending order.
 # Then, keep just the district name, Algebra I proficiency, and CORE region variables.
 
-
+ach_profile %>%
+    arrange(CORE_region, desc(AlgI)) %>%
+    select(system_name, AlgI, CORE_region)
 
 ## Exercise 6
 # Use `summarise()` to find the mean, minimum, and maximum district grad rate.
 # Assign variable names to the resulting data frame.
-
-
+ach_profile %>%
+    filter(system_name != "State of Tennessee") %>%
+    summarise(mean_grad = mean(Graduation, na.rm = TRUE),
+              min_grad = min(Graduation, na.rm = TRUE),
+              max_grad = max(Graduation, na.rm = TRUE))
 
 ## Exercise 7
 # Identify districts with a higher Percent ED than the median district, and a
 # higher Math proficiency than the median district.
 
-
+ach_profile %>%
+    filter(Math > median(Math,na.rm = TRUE),
+           Pct_ED > median(Pct_ED,na.rm = TRUE)) %>%  View()
 
 # Exercise 8
 # Identify districts with a higher dropout rate than the average of districts
 # in the same CORE Region.
 
+ach_profile %>%
+    group_by(CORE_region) %>%
+    filter(Dropout > mean(Dropout, na.rm = TRUE)) %>%
+    ungroup() %>%
+    View()
 
 
 ## Exercise 9
@@ -66,14 +108,28 @@ ach_profile <- read_csv("data/achievement_profile_data_with_CORE.csv")
 # * The English average is next to the individual English variables.
 # * The science average is next to the individual science variables.
 
-
+ach_profile %>%
+    rowwise() %>%
+    mutate(Math_avg = mean(c(Math, AlgI, AlgII), na.rm = TRUE),
+           Eng_avg = mean(c(ELA, EngI, EngII, EngIII), na.ram = TRUE),
+           Sc_avg = mean(c(Science,BioI,Chemistry), na.rm = TRUE)) %>%
+    select(system, system_name, Math, AlgI, AlgII, Math_avg,
+           ELA, EngI, EngII, EngIII, Eng_avg, Science, BioI, Chemistry, Sc_avg) %>%
+    View()
 
 ## Exercise 10
 # Create a data frame with the number of districts at each TVAAS level, by CORE region.
-
+ach_profile %>%
+    inner_join(tvaas, by = "system") %>%
+    group_by(CORE_region,`TVAAS Composite`) %>%
+    count() %>%
+    View()
 
 
 ## Exercise 11
 # Reshape the `tvaas` data frame long by subject, then arrange by system.
 
+tvaas %>%
+    gather(subject, level, `TVAAS Composite`:`TVAAS Numeracy`) %>%
+    arrange(system) %>% View()
 
